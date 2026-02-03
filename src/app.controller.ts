beae9494078710ae0256f6593b9111f07ae96eaa
Artur Mak <none>
2026-02-03 22:37:42 +0200

    feat: add Telegram auth, JWT and Users module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly prisma: PrismaService) {}
-
   @Get()
   getHello(): string {
-    return 'Hello from Magazuno Backend (Db Connected)!';
-  }
-
-  // Создать юзера (Тест)
-  @Post('users/test')
-  async createTestUser(
-    @Body() body: { telegram_id: string; username: string },
-  ) {
-    return this.prisma.user.create({
-      data: {
-        telegram_id: body.telegram_id,
-        username: body.username,
-        role: 'USER',
-      },
-    });
-  }
-
-  // Получить всех юзеров (Тест)
-  @Get('users')
-  async getUsers() {
-    return this.prisma.user.findMany();
+    return 'Hello from Magazuno Backend (System works)!';
   }
 }
